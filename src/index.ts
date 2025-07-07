5fb71ad6cafdefae6477e248b894f73e38269848
Ronald Kiprotich <kironlang@gmail.com>
2025-07-08 00:46:05 +0300

    installed express

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-export const sum = (a: number, b: number): number => {
-    return a + b;
-}
+import express from 'express';
 
-console.log(sum(2,4))
+const app = express();
+app.use(express.json()); //used to parse JSON bodies
 
+// routes
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+})
+
+app.listen(8081, () => {
+    console.log('Server is running on http://localhost:8081');
+}) 
